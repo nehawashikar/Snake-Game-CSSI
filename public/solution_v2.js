f526b62f4b9df4fdb11a1109b964f489841f38db
nehawashikar <neha.washikar@gmail.com>
2021-08-06 00:09:22 -0700

    hello

--- public/solution_v2.js
+++ public/solution_v2.js
@@ -1,112 +1,177 @@
 // Name any p5.js functions we use in `global` so Glitch can recognize them.
 /* global
  *    HSB, background, color, collideRectRect, colorMode, createCanvas, fill, frameRate, keyCode, height,
- *    loop, noFill, noLoop, noStroke, random, rect, round, stroke, sqrt, text, width
- *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW
+ *    loop, noFill, noLoop, noStroke, random, rect, round, stroke, sqrt, text, width, createButton,
+ *    UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, ml5, textSize, strokeWeight, collidePointTriangle,
+ *    BACKSPACE, triangle,
  */
 
-// Solution: Version 2 - With Tail
-
-let backgroundColor, playerSnake, currentApple, score, lives, level1, level2, level3;
+let backgroundColor, playerSnake, currentApple, score, lives, level1, level2, level3,
+    button1, button2, button3, power, power2, obs, gameIsOver, time, counter, home,
+    rect1, rect2, rect3, rect4, rect5, rect6, rect7, rect8, rect9, rect10, rect11,
+    rect12, rect13, rect14, rect15, rect16, rect17, hitRect1,  hitRect2, hitRect3,
+    hitRect4, hitRect5, hitRect6, hitRect7, hitRect8, hitRect9, hitRect10, hitRect11,
+    hitRect12, hitRect13, hitRect14, hitRect15, hitRect16, hitRect17;
 let label = 'listening...';
 let classifier;
 let imageModelURL = 'https://teachablemachine.withgoogle.com/models/aaRjZ9wBL/';
 
 classifier = ml5.soundClassifier(imageModelURL + 'model.json');
 
+let id;
+/*function mouseDragged(){
+  console.log('Sending: ' + mouseX + ';' + mouseY);
+
+
+
+  noStroke();
+  fill(60, 50, 100);
+  ellipse(mouseX, mouseY, 36, 36);
+}
+
+function stroke2(data){
+  noStroke();
+  fill(255, 50, 100);
+  ellipse(data.x, data.y, 36, 36);
+}*/
+
 function setup() {
 
   createCanvas(500, 500);
   colorMode(HSB, 360, 100, 100);
   backgroundColor = 95;
-  frameRateChanger = 6;
-  frameRate(frameRateChanger);
-  
-  classifier.classify(gotResult);
-  
-  button1 = createButton('Level 1 - EASY');
-  button1.position(230, 260);
-  button1.mousePressed(levelOne);
-  
-  button2 = createButton('Level 2 - MEDIUM');
-  button2.position(220, 290);
-  button2.mousePressed(levelTwo);
-  
-  button3 = createButton('Level 3 - HARD');
-  button3.position(230, 320);
-  button3.mousePressed(levelThree);
-  
+  frameRate(6);
+
+  classifier.classify(gotResult); // voice control
+  socket = io.connect('http://localhost:3000') // add web socket
+
   playerSnake = new Snake();
+  let data = {
+    x: playerSnake.x,
+    y: playerSnake.y,
+    size: playerSnake.size,
+    direction: playerSnake.direction,
+    speed: playerSnake.speed,
+    tail: playerSnake.tail
+  }
+  socket.emit('start', data)
+
+  socket.on('heartbeat',
+    function(data){
+      //console.log(data);
+      snakes = data;
+    }
+  );
+
+//level 1 button
+  button1 = createButton('Level 1 - EASY');
+  button1.position(230, 230);
+  button1.mousePressed(levelOne);
+
+// level 2 button
+  button2 = createButton('Level 2 - MEDIUM');
+  button2.position(220, 260);
+  button2.mousePressed(levelTwo);
+
+//level 3 button
+  button3 = createButton('Level 3 - HARD');
+  button3.position(230, 290);
+  button3.mousePressed(levelThree);
+
+//create  an apple
   currentApple = new Apple();
-  
+
+//create power ups
   power = [];
   for (let i = 0; i < 3; i++){
-    let p = new PowerUpScore();
+    let p = new PowerUpTime();
     power.push(p);
   }
-  
+
+  //create power ups
   power2 = [];
   for (let i = 0; i < 3; i++){
     let p2 = new PowerUpResetLives();
     power2.push(p2);
   }
-  
-  
+
+  //create obstacles
   obs = [];
     for (let i = 0; i < 3; i++){
       let o = new Obstacle();
       obs.push(o);
     }
-  
+
   gameIsOver = false;
   score = 0;
   lives = 5;
   time = 0;
-  
+  counter = 3;
+
   home = true;
   level1 = false;
   level2 = false;
   level3 = false;
-  
+
 }
 
 function draw() {
-  background(backgroundColor); //random(50,100)
-  
+  background(backgroundColor);
+
+// set the home screen
   if(home){
     homeScreen();
   }else if(!home){
+    playerSnake.moveSelf();
+    playerSnake.showSelf();
+    playerSnake.checkCollisions();
+    playerSnake.checkApples();
+
+    for (let i = 0; i < snakes.length; i++){
+      let id = snakes[i].id
+      if (id.substring(2, id.length) !== socket.id)
+        stroke(240, 100, 100);
+        noFill();
+        rect(snakes[i].x, snakes[i].y, snakes[i].size, snakes[i].size);
+        noStroke();
+        for (let i = 0; i < playerSnake.tail.length; i++) {
+          playerSnake.tail[i].showSelf();
+    }
+    let data = {
+      x: playerSnake.x,
+      y: playerSnake.y,
+      size: playerSnake.size,
+      direction: playerSnake.direction,
+      speed: playerSnake.speed,
+      tail: playerSnake.tail
+    }
+    socket.emit('update', data)
+
+    currentApple.showSelf();
+    displayText();
+    displayNoise();
+    handleTime();
     if (level1 || level2) {
-      playerSnake.moveSelf();
-      playerSnake.showSelf();
-      playerSnake.checkCollisions();
-      playerSnake.checkApples();
-      currentApple.showSelf();
       powerAndObs();
-      displayText();
-      displayNoise();
-      handleTime();
-      
-    } else if (level3) {
-        mazeCreation();
-        playerSnake.moveSelf();
-        playerSnake.showSelf();
-        playerSnake.checkCollisions();
-        playerSnake.checkApples();
-        currentApple.showSelf();
-    
-        displayText();
-        displayNoise();
-        handleTime();
+    }else if (level3) {
+      mazeCreation();
+
     }
   }
-  
-  if(lives <= 0 || gameIsOver){
+// when to win game
+  if(lives > 0 && score >= 10 && counter > 0){
+    fill(random(360),50,100);
+    winGame();
+  }
+
+//when to lose game
+  if(lives <= 0 || gameIsOver || counter < 0 || time >= 1000){
     gameOver();
     gameIsOver = false;
+    counter = 3;
+    }
   }
 }
-
 
 function levelOne(){
   level1 = true;
@@ -131,9 +196,9 @@ function levelThree(){
   button2.position(0-200, 0);
   button3.position(0-200, 0);
 }
-
+// set the home screen
 function homeScreen(){
-  
+
     fill(120,100,20);
     rect(0,0,width,height);
     textSize(60);
@@ -144,7 +209,7 @@ function homeScreen(){
     noStroke();
     strokeWeight(2);
     textSize(12);
-    
+
     fill(0);
     rect(70,100,40);
     rect(115,100,40);
@@ -157,22 +222,25 @@ function homeScreen(){
 
     fill(0,100,100);
     textSize(20);
-    text('CHOOSE A LEVEL',160,200);
+    text(' CHOOSE A LEVEL',160,180);
     textSize(12);
     fill(100);
-    text('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -',10,height-160);
-    text('                                   Press the SPACE bar to restart current level',10,height-140);
-    text('  Press the SPACE bar then the BACKSPACE bar to restart game and return to this page',10,height-120);
-    text('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -',10,height-100);
-    text('                    Bright colorful squares are powerups used to increase score by 2',10,height-80);
-    text('                    Bright colorful triangles are powerups used to reset lives back to 5',10,height-60);
-    text('Dim colorful rectangles that are differently shaped are obstacles that reset score back to 0',10,height-40);
-    text('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -',10,height-20);
-  
-}
+    text('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -',10,height-200);
+    text('Press the SPACE bar to restart current level',140,height-180);
+    text('Press the SPACE bar then the BACKSPACE bar to restart game and return to this page',17,height-160);
+    text('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -',10,height-140);
+    text('*** FOR LEVELS 1 & 2 ***',180,height-120);
+    text('Bright colorful squares are powerups used to increase score by 2',75,height-100);
+    text('Bright colorful triangles are powerups used to reset lives back to 5',75,height-80);
+    text('Dim colorful rectangles that are differently shaped are obstacles that reset score back to 0',10,height-60);
+    text('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -',10,height-40);
+    text('*** FOR LEVEL 3 ***',195,height-25);
+    text('If you collide with the maze lines 3 times, the game will be over',85,height-10);
 
+}
+// create the maze + check collisions
 function mazeCreation(){
-  
+
   fill(0);
   rect1 = rect(250, 0, 3, 180);
   rect2 = rect(80, 180, 250, 3);
@@ -191,7 +259,7 @@ function mazeCreation(){
   rect15 = rect(419, 80, 3, 73);
   rect16 = rect(420, 400, 80, 3);
   rect17 = rect(420, 260, 3, 144);
-  
+
   hitRect1 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 250, 0, 3, 180);
   hitRect2 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 80, 180, 250, 3);
   hitRect3 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 80, 100, 75, 3);
@@ -208,35 +276,41 @@ function mazeCreation(){
   hitRect14 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 330, 80, 89, 3);
   hitRect15 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 419, 80, 3, 73);
   hitRect16 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 420, 400, 80, 3);
-  hitRect17 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 420, 260, 3, 144); 
-  
-  if(hitRect1 || hitRect2 || hitRect3 || hitRect4 || hitRect5 || hitRect6 || hitRect7 || hitRect8 || hitRect9 
-     || hitRect10 || hitRect11 || hitRect12 || hitRect13 || hitRect14 || hitRect15 || hitRect16 || hitRect17){
-    lives--;
-    restartGame();
-  }
-}
+  hitRect17 = collideRectRect(playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size, 420, 260, 3, 144);
 
+    if(hitRect1 || hitRect2 || hitRect3 || hitRect4 || hitRect5 || hitRect6 || hitRect7 || hitRect8 || hitRect9
+       || hitRect10 || hitRect11 || hitRect12 || hitRect13 || hitRect14 || hitRect15 || hitRect16 || hitRect17){
+      restartGame();
+      counter--;
+    }
+  }
+
+//display scores
 function displayText() {
   fill(0);
   text(`Your score is: ${score}`, 5, 15);
-  text(`You have ${lives} lives left`, 5, 30);
+  if(level1 || level2){
+    text(`You have ${lives} lives left`, 5, 30);
+  }else if(level3){
+    text(`You have ${counter} lives left`, 5, 30);
+  }
 }
-
+//display noise inputs
 function displayNoise(){
   fill(0);
   text('*' + label + '*', 5, height - 4);
 }
-
+//get noise inputs
 function gotResult(error, results){
   if(error){
     console.error('error');
     return;
   }
-  label = results[0].label; 
+  label = results[0].label;
   controlSnake();
 }
 
+// voice control of the snake
 function controlSnake(){
   if (label === 'Up' && playerSnake.direction != "S") {
     playerSnake.direction = "N";
@@ -246,9 +320,10 @@ function controlSnake(){
     playerSnake.direction = "E";
   } else if (label === 'Left' && playerSnake.direction != "E") {
     playerSnake.direction = "W";
-  }  
+  }
 }
 
+// track the time per level
 function handleTime(){
   fill(color(20,100,100));
   text(`Time taken in this level: ${time}`, 150, 20);
@@ -257,38 +332,54 @@ function handleTime(){
 
 function powerAndObs(){
   for (let powers of power){
+    // create rectangle power up objects
     powers.move();
     powers.display();
-    
+
+    //level 1 power up: more time
     if (level1){
     let scoreInc = collideRectRect(powers.x, powers.y, 15, 15, playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size);
       if(scoreInc){
-        score += 2;
+        if(time > 200){
+          time -= 200;
+        } else{
+          time = 0;
+        }
+        //change the x and y variable of the time power
         powers.x = random(width);
         powers.y = random(height);
       }
-    } else if (level2){
+    }
+    //level 2 power up: more time (power ups are smaller)
+    else if (level2){
       let scoreInc = collideRectRect(powers.x, powers.y, 7, 7, playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size);
       if(scoreInc){
-        score += 2;
+        if(time > 200){
+          time -= 200;
+        } else{
+          time = 0;
+        }
+        //change the x and y variable of the time power
         powers.x = random(width);
         powers.y = random(height);
       }
     }
   }
-  
+
   for (let powers2 of power2){
+    // create triangle power up objects
     powers2.move();
     powers2.display();
-    
+
     let livesReset1 = collidePointTriangle(playerSnake.x, playerSnake.y, powers2.x1, powers2.y1, powers2.x2, powers2.y2, powers2.x3, powers2.y3);
     let livesReset2 = collidePointTriangle(playerSnake.x + playerSnake.size, playerSnake.y, powers2.x1, powers2.y1, powers2.x2, powers2.y2, powers2.x3, powers2.y3);
     let livesReset3 = collidePointTriangle(playerSnake.x, playerSnake.y + playerSnake.size, powers2.x1, powers2.y1, powers2.x2, powers2.y2, powers2.x3, powers2.y3);
     let livesReset4 = collidePointTriangle(playerSnake.x + playerSnake.size, playerSnake.y + playerSnake.size, powers2.x1, powers2.y1, powers2.x2, powers2.y2, powers2.x3, powers2.y3);
-    
-    
+
+
     if (level1) {
       if(livesReset1 || livesReset2 || livesReset3 || livesReset4){
+        // reset lives = 5, randomize powerup position
         lives = 5;
         powers2.x1 = random(width);
         powers2.y1 = random(height);
@@ -299,6 +390,7 @@ function powerAndObs(){
       }
     } else if (level2) {
       if(livesReset1 || livesReset2 || livesReset3 || livesReset4){
+        // reset lives = 5, randomize smaller powerup position
         lives = 5;
         powers2.x1 = random(width);
         powers2.y1 = random(height);
@@ -309,13 +401,16 @@ function powerAndObs(){
       }
     }
   }
-  
+
   for (let theObs of obs){
+    // create the obstacles
     theObs.move();
     theObs.display();
-    
+
     let scoreInc = collideRectRect(theObs.x, theObs.y, theObs.rectWidth, theObs.rectHeight, playerSnake.x, playerSnake.y, playerSnake.size, playerSnake.size);
+
     if (level1) {
+      // decrease score to 0, randomize
       if(scoreInc){
         score = 0;
         theObs.x = random(width);
@@ -324,6 +419,7 @@ function powerAndObs(){
         theObs.rectHeight = random(5,10);
       }
     } else if (level2) {
+      // decrease score to 0, randomize --> bigger obstacles
       if(scoreInc){
         score = 0;
         theObs.x = random(width);
@@ -336,7 +432,7 @@ function powerAndObs(){
 }
 
 function keyPressed() {
-  
+  // key controls for player Snake (playaer 1)
   if (keyCode === UP_ARROW && playerSnake.direction != 'S') {
     playerSnake.direction = "N";
   }else if (keyCode === DOWN_ARROW && playerSnake.direction != 'N') {
@@ -346,15 +442,17 @@ function keyPressed() {
   }else if (keyCode === LEFT_ARROW && playerSnake.direction != 'E') {
     playerSnake.direction = "W";
   }else if (keyCode === 32) {
+    // reset to level __
     restartGame();
   }else if (keyCode === BACKSPACE) {
+    // reset to homescreen
     home = true;
     level1 = false;
     level2 = false;
     level3 = false;
-    button1.position(230, 260);
-    button2.position(220, 290);
-    button3.position(230, 320);
+    button1.position(230, 230);
+    button2.position(220, 260);
+    button3.position(230, 290);
   }
 }
 
@@ -362,18 +460,35 @@ function restartGame() {
   score = 0;
   lives = 5;
   time = 0;
-  frameRateChanger = 6;
   playerSnake = new Snake();
   currentApple = new Apple();
   loop();
 }
 
 function gameOver() {
-  stroke(0);
-  fill(0);
-  text("GAME OVER", 50, 50);
   gameIsOver = true;
+  stroke(0);
+  fill(250,100,20);
+  rect(0,0,width,height)
+  fill(100);
+  textSize(50);
+  text("GAME OVER", 90, 150);
+  text(`Final Score: ${score}`,90,210);
+  textSize(12);
+  text('Press the SPACE bar to restart current level',140,height-180);
+  text('Press the SPACE bar then the BACKSPACE bar to restart game and return to this page',17,height-160);
   noLoop();
+}
+
+function winGame(){
+  rect(0,0,width,height);
+  textSize(50);
+  fill(100);
+  text('YOU WIN!',125,150);
+  textSize(12);
+  text('Try to win in another level', 172,height-200);
+  text('Press the SPACE bar to restart current level',140,height-180);
+  text('Press the SPACE bar then the BACKSPACE bar to restart game and return to this page',17,height-160);
 }
 
 class Snake {
@@ -401,7 +516,7 @@ class Snake {
     this.tail.unshift(new TailSegment(this.x, this.y));
     this.tail.pop();
   }
-  
+
   showSelf() {
     stroke(240, 100, 100);
     noFill();
@@ -414,44 +529,50 @@ class Snake {
 
   checkApples() {
     if (collideRectRect(this.x, this.y, this.size, this.size,
-        currentApple.x, currentApple.y, currentApple.size, currentApple.size)) {
+      currentApple.x, currentApple.y, currentApple.size, currentApple.size)) {
       score += 1;
       currentApple = new Apple();
       this.extendTail();
-      if(level2){
-        this.speed += 3;
+    }
+    if (level2) {
+      if (time % 40 == 0) {
+        currentApple = new Apple();
       }
     }
   }
 
   checkCollisions() {
-    
+
     //collisions with the canvas borders
     if (this.y >= height) {
       lives--;
-      this.y = this.size; 
+      counter--;
+      this.y = this.size;
       this.direction = "S";
       currentApple = new Apple();
     }
     if (this.y <= 0) {
       lives--;
-      this.y = height - this.size; 
+      counter--;
+      this.y = height - this.size;
       this.direction = "N";
       currentApple = new Apple();
     }
     if (this.x >= width) {
       lives--;
-      this.x = this.size; 
+      counter--;
+      this.x = this.size;
       this.direction = "E";
       currentApple = new Apple();
     }
     if (this.x <= 0) {
       lives--;
-      this.x = width - this.size; 
+      counter--;
+      this.x = width - this.size;
       this.direction = "W";
       currentApple = new Apple();
     }
-    
+
     //collision of snake with itself
     if (this.tail.length > 2) {
       for (let i=1; i < this.tail.length; i++) {
@@ -460,7 +581,7 @@ class Snake {
         }
         text(i, this.tail[i].x, this.tail[i].y)
       }
-    } 
+    }
   }
 
   extendTail() {
@@ -497,7 +618,8 @@ class Apple {
   }
 }
 
-class PowerUpScore{
+// for the time power up
+class PowerUpTime{
   constructor(){
     this.x = random(width);
     this.y = random(height);
@@ -516,7 +638,7 @@ class PowerUpScore{
       }
      }
   }
-  
+
   display(){
     fill(random(360), 80, 100);
     if (level1) {
@@ -526,8 +648,8 @@ class PowerUpScore{
     }
   }
 }
-  
 
+// for the lives power  up
 class PowerUpResetLives{
   constructor(){
     this.x1 = random(width);
@@ -564,6 +686,7 @@ class PowerUpResetLives{
   }
 }
 
+// for the score reducer obastacle
 class Obstacle{
   constructor(){
     this.x = random(width);
@@ -578,7 +701,7 @@ class Obstacle{
         this.y = random(height);
         this.rectWidth = random(10,16);
         this.rectHeight = random(5,10);
-      } 
+      }
     } else if (level2) {
       if (time % 7 == 0 && time > 0){
         this.x = random(width);
@@ -589,7 +712,7 @@ class Obstacle{
     }
   }
   display(){
-    fill(random(360), 100, 40);
+    fill(270, 100, 40);
     rect(this.x, this.y, this.rectWidth, this.rectHeight);
   }
 }
--- server.js
+++ server.js
@@ -1,1 +1,65 @@
-co
+
+const express = require("express");
+const socket = require("socket.io"); // import statement
+
+let snakes = [];
+
+class Snake{
+  constructor(id, x, y, direction, size, speed, tail){
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.size = size;
+    this.speed = speed;
+  }
+}
+
+let app = express();
+let server = app.listen(3000);
+
+app.use(express.static('public')); //"static" to host static files
+
+console.log("my scocket server is running");
+
+let io = socket(server); // const
+
+setInterval(heartbeat, 33);
+
+function heartbeat(){
+  io.sockets.emit('heartbeat', snakes);  // ... goes to everyone INCLUDING this client
+}
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log('new connection: ' + socket.id);
+
+  socket.on('start',
+    function(data){
+      console.log(`${socket.id} ${data.x} ${data.y} ${data.direction} ${data.size} ${data.speed}`);
+      let snake = new Snake(socket.id, data.x, data.y, data.direction, data.size,data.speed)
+      snakes.push(snake);
+      //socket.broadcast.emit('mouse', data); // goes to everyone exclusing this client
+
+      //console.log(data);
+    }
+  );
+  socket.on('update', function(data){
+    //console.log(`${socket.id} ${data.x} ${data.y} ${data.direction} ${data.size} ${data.speed}`);
+
+    let snake;
+    for (let i=0; i <snakes.length; i++){
+      if (socket.id == snakes[i].id){
+        snake = snakes[i];
+      }
+    }
+
+    snake.x = data.x;
+    snake.y = data.y;
+    snake.direction = data.direction;
+    snake.size = data.size;
+    snake.speed = data.speed;
+    }
+  );
+};
